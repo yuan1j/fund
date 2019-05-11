5e8cb3b9e64acd3f253140eb09f973b457ae9f41
yuan1j <1069645408@qq.com>
2019-05-11 17:50:45 +0800

    版本2

--- fund/middlewares.py
+++ fund/middlewares.py
@@ -4,11 +4,107 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+import re
 import time
-
 from scrapy import signals
 from scrapy.http import HtmlResponse
 from selenium import webdriver
+from sphinx.util import requests
+
+
+class FundSpiderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, dict or Item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Response, dict
+        # or Item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class FundDownloaderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+        return None
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
 
 
 class SeleniumDownloadMiddleware(object):
@@ -16,13 +112,18 @@ class SeleniumDownloadMiddleware(object):
         self.driver = webdriver.Chrome()
 
     def process_request(self, request, spider):
-        print("当前请求的url为：" + request.url)
-        if request.url.endswith("next"):
-            self.driver.find_element_by_xpath('//div[@id="pagebar"]/label[last()]').click()
-            time.sleep(2)
+        if re.match('.*hbxfundranking.*', request.url):
+            print("当前请求的url为：" + request.url)
+            if request.url.endswith("next"):
+                self.driver.find_element_by_xpath('//div[@id="pagebar"]/label[text()="下一页"]').click()
+                time.sleep(2)
+                print("翻页")
+            else:
+                self.driver.get(request.url)
+                time.sleep(5)
+            response = HtmlResponse(url=self.driver.current_url, body=self.driver.page_source, request=request,
+                                    encoding="utf8")
+            return response
         else:
-            self.driver.get(request.url)
-            time.sleep(5)
-        source = self.driver.page_source
-        response = HtmlResponse(url=self.driver.current_url, body=source, request=request, encoding="utf8")
-        return response
+            response = requests.get(request.url)
+            return HtmlResponse(url=request.url, body=response.content, request=request, encoding="utf8")
--- fund/spiders/monetary_fund.py
+++ fund/spiders/monetary_fund.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+
 import scrapy
 from fund.items import FundItem
 
@@ -7,17 +9,17 @@ class MonetaryFundSpider(scrapy.Spider):
     name = 'monetary_fund'
     allowed_domains = ['eastmoney.com']
     start_urls = ['http://fund.eastmoney.com/data/hbxfundranking.html#t;c0;r;sTEYI;ddesc;pn50;mg;os1;']
+    pages = 1
 
     def parse(self, response):
-        # 获取item对象
-        item = FundItem()
         # 分组
         tr_list = response.xpath('//table[@id="dbtable"]/tbody/tr')
-        print(tr_list)
         # 　遍历
         for tr in tr_list:
+            # 获取item对象
+            item = FundItem()
             item["fund_code"] = tr.xpath("./td[3]/a/text()").extract_first()
-            item["fund_name"] = tr.xpath("./td[4]/a/text()").extract_first()
+            item["fund_name"] = tr.xpath("./td[4]/a/@title").extract_first()
             item["pub_date"] = tr.xpath("./td[5]/text()").extract_first()
             item["profit_per10000"] = tr.xpath("./td[6]/text()").extract_first()
             item["annual_return_7th"] = tr.xpath("./td[7]/text()").extract_first()
@@ -35,21 +37,25 @@ class MonetaryFundSpider(scrapy.Spider):
             item["rate_all"] = tr.xpath("./td[19]/text()").extract_first()
 
             url_detail = tr.xpath("./td[4]/a/@href").extract_first()
-            # print(item["fund_code"])
             print(item["fund_name"])
-            # print(url_detail)
             yield scrapy.Request(url_detail, callback=self.parse_detail, meta={"item": item})
 
         # 构造下一页url
-        if response.xpath('//div[@id="pagebar"]/label[last()]/@class').extract_first() != "end":
-            yield scrapy.Request(self.start_urls[0] + "text", callback=self.parse)
+        next_class = response.xpath('//div[@id="pagebar"]/label[last()]/@class').extract_first()
+        print(next_class)
+        if next_class != "end":
+            self.pages += 1
+            print("正在爬取第" + str(self.pages) + "页的数据")
+            #  dont_filter 设置为True，表示不过滤重复网页
+            yield scrapy.Request(self.start_urls[0] + "next", callback=self.parse, dont_filter=True)
 
     def parse_detail(self, response):
         item = response.meta["item"]
         item["fund_size"] = response.xpath(
-            '//div[@class="infoOfFund"]/table/tbody/tr[1]/td[2]/a/text()').extract_first()
+            '//div[@class="infoOfFund"]/table/tr[1]/td[2]/text()').extract_first()
+        item["fund_size"] = re.search("：(.*?)（", item["fund_size"]).group(1)
         item["fund_manager"] = response.xpath(
-            '//div[@class="infoOfFund"]/table/tbody/tr[1]/td[3]/a/text()').extract_first()
+            '//div[@class="infoOfFund"]/table/tr[1]/td[3]/a/text()').extract_first()
         item["fund_company"] = response.xpath(
-            '//div[@class="infoOfFund"]/table/tbody/tr[2]/td[2]/a/text()').extract_first()
+            '//div[@class="infoOfFund"]/table/tr[2]/td[2]/a/text()').extract_first()
         yield item
